05f82c8b53c701f7ba09fa2a6be60b40d5f3f05d
Brooks Townsend <brooksmtownsend@gmail.com>
2024-05-06 17:27:35 -0400

    feat(*): update handlers for 1.0
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- cmd/blank/main.go
+++ cmd/blank/main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/wasmCloud/provider-sdk-go"
 )
 
 func main() {
-	fmt.Println("Initializing provider ...")
 	p, err := provider.New(
-		// provider.WithProviderActionFunc(handleKVAction),
-		// provider.WithNewLinkFunc(handleNewLink),
-		// provider.WithDelLinkFunc(handleDelLink),
-		// provider.WithHealthCheckMsg(healthCheckMsg),
+	// provider.WithProviderActionFunc(handleKVAction),
+	// provider.WithNewLinkFunc(handleNewLink),
+	// provider.WithDelLinkFunc(handleDelLink),
+	// provider.WithHealthCheckMsg(healthCheckMsg),
 	)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Starting provider ...")
 	err = p.Start()
 	if err != nil {
 		panic(err)
--- models.go
+++ models.go
@@ -27,8 +27,8 @@ type Topics struct {
 	LATTICE_LINK_GET string
 	LATTICE_LINK_DEL string
 	LATTICE_LINK_PUT string
-	LATTICE_SHUTDOWN    string
-	LATTICE_HEALTH      string
+	LATTICE_SHUTDOWN string
+	LATTICE_HEALTH   string
 }
 
 func LatticeTopics(h HostData) Topics {
@@ -36,8 +36,8 @@ func LatticeTopics(h HostData) Topics {
 		LATTICE_LINK_GET: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.get", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
 		LATTICE_LINK_DEL: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.del", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
 		LATTICE_LINK_PUT: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.put", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_SHUTDOWN:    fmt.Sprintf("wasmbus.rpc.%s.%s.%s.shutdown", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_HEALTH:      fmt.Sprintf("wasmbus.rpc.%s.%s.%s.health", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
+		LATTICE_SHUTDOWN: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.shutdown", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
+		LATTICE_HEALTH:   fmt.Sprintf("wasmbus.rpc.%s.%s.%s.health", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
 	}
 }
 

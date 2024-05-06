8068d99b31a42c37f019baa69fac7ece561f3aff
Brooks Townsend <brooksmtownsend@gmail.com>
2024-05-06 17:33:14 -0400

    refactor(*): remove old structures
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- models.go
+++ models.go
@@ -2,26 +2,7 @@ package provider
 
 import (
 	"fmt"
-
-	"github.com/golang-jwt/jwt"
 )
-
-type Claims struct {
-	jwt.StandardClaims
-	ID     string `json:"jti"`
-	Wascap Wascap `json:"wascap"`
-}
-
-type Wascap struct {
-	TargetURL string `json:"target_url"`
-	OriginURL string `json:"origin_url"`
-	Hash      string `json:"hash"`
-}
-
-type ProviderResponse struct {
-	Msg   []byte `msgpack:"msg,omitempty"`
-	Error string `msgpack:"error,omitempty"`
-}
 
 type Topics struct {
 	LATTICE_LINK_GET string
@@ -39,11 +20,4 @@ func LatticeTopics(h HostData) Topics {
 		LATTICE_SHUTDOWN: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.shutdown", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
 		LATTICE_HEALTH:   fmt.Sprintf("wasmbus.rpc.%s.%s.%s.health", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
 	}
-}
-
-type ProviderAction struct {
-	Operation string
-	Msg       []byte
-	FromActor string
-	// Respond   chan ProviderResponse
 }
--- nats.go
+++
@@ -1,4 +0,0 @@
-package provider
-
-// The 1.0 version of the wasmCloud control API, used in topic strings for the control API
-const CTL_API_VERSION_1 string = "v1"

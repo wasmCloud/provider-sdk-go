30dc11f1b26ca1c16a2ce80b9c5f5180a0f49870
Brooks Townsend <brooksmtownsend@gmail.com>
2024-05-08 12:49:06 -0400

    fix: support b64 encoded hostdata
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- models.go
+++ models.go
@@ -14,10 +14,10 @@ type Topics struct {
 
 func LatticeTopics(h HostData) Topics {
 	return Topics{
-		LATTICE_LINK_GET: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.get", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_LINK_DEL: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.del", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_LINK_PUT: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.linkdefs.put", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_SHUTDOWN: fmt.Sprintf("wasmbus.rpc.%s.%s.%s.shutdown", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
-		LATTICE_HEALTH:   fmt.Sprintf("wasmbus.rpc.%s.%s.%s.health", h.LatticeRPCPrefix, h.ProviderKey, h.LinkName),
+		LATTICE_LINK_GET: fmt.Sprintf("wasmbus.rpc.%s.%s.linkdefs.get", h.LatticeRPCPrefix, h.ProviderKey),
+		LATTICE_LINK_DEL: fmt.Sprintf("wasmbus.rpc.%s.%s.linkdefs.del", h.LatticeRPCPrefix, h.ProviderKey),
+		LATTICE_LINK_PUT: fmt.Sprintf("wasmbus.rpc.%s.%s.linkdefs.put", h.LatticeRPCPrefix, h.ProviderKey),
+		LATTICE_HEALTH:   fmt.Sprintf("wasmbus.rpc.%s.%s.health", h.LatticeRPCPrefix, h.ProviderKey),
+		LATTICE_SHUTDOWN: fmt.Sprintf("wasmbus.rpc.%s.%s.default.shutdown", h.LatticeRPCPrefix, h.ProviderKey),
 	}
 }

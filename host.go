7ce19a48bc29a5732f9ea751626e726b56adb693
Brooks Townsend <brooksmtownsend@gmail.com>
2024-07-19 16:07:21 -0400

    wip: feat(provider): add support for secrets and xkeys
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- host.go
+++ host.go
@@ -9,24 +9,28 @@ type OtelConfig struct {
 	TracesEndpoint        string `json:"traces_endpoint,omitempty"`
 	MetricsEndpoint       string `json:"metrics_endpoint,omitempty"`
 	LogsEndpoint          string `json:"logs_endpoint,omitempty"`
+	TraceLevel            *Level `json:"trace_level,omitempty"`
 }
 
 type HostData struct {
-	HostID              string                    `json:"host_id,omitempty"`
-	LatticeRPCPrefix    string                    `json:"lattice_rpc_prefix,omitempty"`
-	LatticeRPCUserJWT   string                    `json:"lattice_rpc_user_jwt,omitempty"`
-	LatticeRPCUserSeed  string                    `json:"lattice_rpc_user_seed,omitempty"`
-	LatticeRPCURL       string                    `json:"lattice_rpc_url,omitempty"`
-	ProviderKey         string                    `json:"provider_key,omitempty"`
-	EnvValues           map[string]string         `json:"env_values,omitempty"`
-	InstanceID          string                    `json:"instance_id,omitempty"`
-	LinkDefinitions     []InterfaceLinkDefinition `json:"link_definitions,omitempty"`
-	ClusterIssuers      []string                  `json:"cluster_issuers,omitempty"`
-	Config              map[string]string         `json:"config,omitempty"`
-	DefaultRPCTimeoutMS *uint64                   `json:"default_rpc_timeout_ms,omitempty"`
-	StructuredLogging   bool                      `json:"structured_logging,omitempty"`
-	LogLevel            *Level                    `json:"log_level,omitempty"`
-	OtelConfig          OtelConfig                `json:"otel_config,omitempty"`
+	HostID                 string                     `json:"host_id,omitempty"`
+	LatticeRPCPrefix       string                     `json:"lattice_rpc_prefix,omitempty"`
+	LatticeRPCUserJWT      string                     `json:"lattice_rpc_user_jwt,omitempty"`
+	LatticeRPCUserSeed     string                     `json:"lattice_rpc_user_seed,omitempty"`
+	LatticeRPCURL          string                     `json:"lattice_rpc_url,omitempty"`
+	ProviderKey            string                     `json:"provider_key,omitempty"`
+	EnvValues              map[string]string          `json:"env_values,omitempty"`
+	InstanceID             string                     `json:"instance_id,omitempty"`
+	LinkDefinitions        []linkWithEncryptedSecrets `json:"link_definitions,omitempty"`
+	ClusterIssuers         []string                   `json:"cluster_issuers,omitempty"`
+	Config                 map[string]string          `json:"config,omitempty"`
+	Secrets                map[string]SecretValue     `json:"secrets,omitempty"`
+	HostXKeyPublicKey      string                     `json:"host_xkey_public_key,omitempty"`
+	ProviderXKeyPrivateKey string                     `json:"provider_xkey_private_key,omitempty"`
+	DefaultRPCTimeoutMS    *uint64                    `json:"default_rpc_timeout_ms,omitempty"`
+	StructuredLogging      bool                       `json:"structured_logging,omitempty"`
+	LogLevel               *Level                     `json:"log_level,omitempty"`
+	OtelConfig             OtelConfig                 `json:"otel_config,omitempty"`
 }
 
 type HealthCheckResponse struct {
